bdd60fdbea98a560a440383b669627f43ba26b99
vanthuc1810 <vanthuc08888@gmail.com>
2024-09-04 17:03:48 +0700

    Updates

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from "redux";
-import rootReducer from "./reducer/rootReducer";
-import { applyMiddleware } from "redux";
-import { composeWithDevTools } from "@redux-devtools/extension";
-import { thunk } from 'redux-thunk';
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-
-export default store;
